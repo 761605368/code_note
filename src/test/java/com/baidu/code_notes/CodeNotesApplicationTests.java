9370ff8c54e9e23d63d71fae9e42d5f2f40489a1
lxh <761605368@qq.com>
2023-02-12 14:17:07 +0800

    java抓取省市区三级行政区域

--- src/test/java/com/baidu/code_notes/CodeNotesApplicationTests.java
+++ src/test/java/com/baidu/code_notes/CodeNotesApplicationTests.java
@@ -1,13 +1,21 @@
 package com.baidu.code_notes;
 
+import com.alibaba.fastjson.JSONObject;
+import com.baidu.code_notes.utils.JavaJsoupUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CodeNotesApplicationTests {
 
     @Test
     void contextLoads() {
+        JavaJsoupUtil util = new JavaJsoupUtil();
+        List<JavaJsoupUtil.SysCitys> sysAreas = util.getProvinces();
+        System.out.println(sysAreas.size());
+        System.err.println("爬虫相应数据为："+ JSONObject.toJSONString(sysAreas));
     }
 
 }
